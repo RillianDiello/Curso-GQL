b697fd44883338de11782feaa85440496d94286a
RillianDiello <rilliansf@gmail.com>
2019-08-19 22:40:20 -0400

    Alterar Usuarios, Inserir Usuarios

--- projeto-mutations/resolvers/Mutation.js
+++ projeto-mutations/resolvers/Mutation.js
@@ -1,10 +1,32 @@
 const {usuarios, proximoId} = require('../data/db')
 
+
+function indiceUsuario(filtro){
+    if(!filtro) return -1
+    const {id, email} = filtro
+    if(id){
+        return usuarios.findIndex(u => u.id === id)
+    }else if(email){
+        return usuarios.findIndex(u => u.email === email)
+    }
+
+    return -1
+}
 module.exports = {
-    novoUsuario(_, {nome, email, idade}){
+    novoUsuario(_, 
+        // {nome, email, idade}
+        args
+        ){
+
+        const emailExistentes = usuarios.some(u => u.email === args.email)
+
+        if(emailExistentes){
+            throw new Error('E-mail já cadastrado')
+        }
+
         const novo = {
             id: proximoId(),
-            nome, email, idade,
+            ...args,
             perfil_id: 1,
             status: 'ATIVO'
         }
@@ -12,5 +34,33 @@ module.exports = {
         usuarios.push(novo)
 
         return novo
+        
+       
+    },
+    excluirUsuario(_, { id }){
+        const i = usuarios.findIndex(u => u.id === id)
+        if(i < 0){
+            throw new Error('Usuario não encontrado')
+        }
+        const excluidos = usuarios.splice(i, 1)
+        return excluidos ? excluidos[0] : null
+    },
+
+    alterarUsuario(_, args){
+        const i = usuarios.findIndex(u => u.id === args.id)
+
+        if(i < 0){
+            throw new Error('Usuario não encontrado')
+        }
+
+        const usuario = {
+            ...usuarios[i],
+            ...args
+        }
+
+        usuarios.splice(i,1,usuario)
+
+        return usuario
+        
     }
 }
