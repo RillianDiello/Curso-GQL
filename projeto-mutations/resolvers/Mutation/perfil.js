8d85e38cd7b8bc9a49318c31f0c51bf0bdf6007b
RillianDiello <rilliansf@gmail.com>
2019-08-21 23:16:01 -0400

    Correçao na exclusao de perfis

--- projeto-mutations/resolvers/Mutation/perfil.js
+++ projeto-mutations/resolvers/Mutation/perfil.js
@@ -1,60 +1,50 @@
-const {perfis, proximoId} = require('../../data/db')
+const { perfis, proximoId } = 
+    require('../../data/db')
 
-function indicePerfil(filtro){
+function indicePerfil(filtro) {
     if(!filtro) return -1
-
-    const {id,nome} = filtro
-    if(id){
-        return perfis.findIndex(p => p.id === filtro.id)
-    }else if(nome){
-        return perfis.findIndex(p => p.nome === filtro.nome)
+    const { id, nome } = filtro
+    if(id) {
+        return perfis
+            .findIndex(p => p.id === id)
+    } else if(nome) {
+        return perfis
+            .findIndex(p => p.nome === nome)
     }
-
+    return -1
 }
 
 module.exports = {
-    novoPerfil(_,
-        {dados}
-        ){
-            const perfilExistente = perfis.some(p => p.nome === dados.nome)
+    novoPerfil(_, { dados }) {
+        const nomeExistente = perfis
+            .some(u => u.nome === dados.nome)
 
-            if(perfilExistente){
-                throw new Error ('Perfil já cadastrado')
-            }
-
-            const novo = {
-                id: proximoId(),
-                ...dados
-            }
-
-            perfis.push(novo)
-
-            return novo
-        },
-
-    excluirPerfil(_,
-        { filtro}
-        ){
-            const i = indicePerfil(filtro)
-
-            if(i < 0){
-                throw new Error('Perfil não encotrado')
-            }
-            const perfisExcluidos = perfis.slice(i,1) //slice vai remover do valor de i até o numero de elmentos de 1
-            return perfisExcluidos ? perfisExcluidos[0] : null
-        },
-    alterarPerfil(_,
-        {filtro, dados}
-        ){
-            const i = indicePerfil(filtro)
-
-            if(i < 0){
-                throw new Error('Perfil não encontrado')
-            }
-
-            perfis[i].nome = dados.nome
-            
-            return perfis[i]
-
+        if(nomeExistente) {
+            throw new Error('Perfil cadastrado')
         }
+
+        const novo = {
+            id: proximoId(),
+            ...dados
+        }
+
+        perfis.push(novo)
+        return novo
+    },
+    excluirPerfil(_, { filtro }) {
+        const i = indicePerfil(filtro)
+        if(i < 0) return null
+        const excluidos = 
+            perfis.splice(i, 1)
+        return excluidos ? 
+            excluidos[0] : null
+    },
+    alterarPerfil(_, { filtro, dados }) {
+        const i = indicePerfil(filtro)
+        if(i < 0) return null
+
+        perfis[i].nome = dados.nome
+        
+        return perfis[i]
+    }
 }
